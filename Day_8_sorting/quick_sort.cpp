7260cbfd09c331e623105e120cc325467c194c36
Rachana <rachanamehta09@gmail.com>
2025-11-17 11:26:04 +0530

    added quick sort

---
+++ Day_8_sorting/quick_sort.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int partition(vector<int> &arr, int low, int high) {
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while (i < j) {
+        while (arr[i] <= pivot && i <= high - 1) i++;
+        while (arr[j] > pivot && j >= low + 1) j--;
+        if (i < j) swap(arr[i], arr[j]);
+    }
+
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+void quickSort(vector<int> &arr, int low, int high) {
+    if (low < high) {
+        int pIndex = partition(arr, low, high);
+        quickSort(arr, low, pIndex - 1);
+        quickSort(arr, pIndex + 1, high);
+    }
+}
+
+int main() {
+    vector<int> arr = {10, 7, 8, 9, 1, 5};
+
+    quickSort(arr, 0, arr.size() - 1);
+
+    cout << "Sorted: ";
+    for (int i = 0; i < arr.size(); i++)
+    cout << arr[i] << " ";
+}
